820c4bd657c969a8023c8eabc2f8867d83ee229e
itsericrao <EricRaoMINECRAFT@outlook.com>
2023-08-28 14:56:08 +0800

    new features & bug fixes

--- main.py
+++ main.py
@@ -26,6 +26,7 @@ img_location = str(script_location) + "\img.png"
 Process Killer
 '''
 def kill():
+    print("LOG >> KeyPressed F9 -> ProcessKilled")
     for proc in psutil.process_iter():
         if proc.name() == PROCNAME:
             proc.kill()
@@ -58,18 +59,20 @@ class GUI(threading.Thread):
         # text labels
         title = Label(self.root, text="GTAOL工具箱", font=("Microsoft YaHei", 24), border=30)
         title.pack()
-        text = Label(self.root, text="F4 快速零食", font=("Microsoft YaHei", 12))
-        text2 = Label(self.root, text="F5 快速防弹衣", font=("Microsoft YaHei", 12))
-        text3 = Label(self.root, text="F9 一键杀进程", font=("Microsoft YaHei", 12))
-        text4 = Label(self.root, text="F11 进程断网", font=("Microsoft YaHei", 12))
-        text5 = Label(self.root, text="F12 进程断网恢复", font=("Microsoft YaHei", 12))
+        text = Label(self.root, text="F3 快速零食", font=("Microsoft YaHei", 12))
+        text2 = Label(self.root, text="F4 快速防弹衣", font=("Microsoft YaHei", 12))
+        text3 = Label(self.root, text="F5 快速叫技工", font=("Microsoft YaHei", 12))
+        text4 = Label(self.root, text="F6 快速叫莱斯特", font=("Microsoft YaHei", 12))
+        text5 = Label(self.root, text="F9 一键杀进程", font=("Microsoft YaHei", 12))
+        text6 = Label(self.root, text="F11 进程断网", font=("Microsoft YaHei", 12))
+        text7 = Label(self.root, text="F12 进程断网恢复", font=("Microsoft YaHei", 12))
         text.pack()
         text2.pack()
         text3.pack()
         text4.pack()
         text5.pack()
-        # text6.pack()
-        # text7.pack()
+        text6.pack()
+        text7.pack()
         self.root.mainloop()
 
 gui = GUI()
@@ -84,7 +87,7 @@ def block():
     time.sleep(0.1)
 
 def unblock():
-    print("LOG >> KeyPressed F22 -> Unblock Internet")
+    print("LOG >> KeyPressed F12 -> Unblock Internet")
     # use terminal to remove firewall rules called "GTAOL". (UAC acquired)
     os.system('netsh advfirewall firewall delete rule name="GTAOL"')
     time.sleep(0.1)
@@ -94,7 +97,7 @@ Macro
 '''
 # Quick Snack
 def snack():
-    print("LOG >> KeyPressed F4 -> QuickSnack")
+    print("LOG >> KeyPressed F3 -> QuickSnack")
     pydirectinput.press('m')
     pydirectinput.press('down', presses=3)
     pydirectinput.press('enter')
@@ -104,7 +107,7 @@ def snack():
 
 # Quick Armor
 def armor():
-    print("LOG >> KeyPressed F6 -> QuickArmor")
+    print("LOG >> KeyPressed F4 -> QuickArmor")
     pydirectinput.press('m')
     pydirectinput.press('down', presses=3)
     pydirectinput.press('enter')
@@ -115,12 +118,14 @@ def armor():
 
 # Quick Mechanic
 def mechanic():
+    print("LOG >> KeyPressed F5 -> QuickMechanic")
     pydirectinput.press('up')
     time.sleep(0.5)
     pydirectinput.press('up')
+    time.sleep(0.1)
     pydirectinput.press('right')
     pydirectinput.press('enter')
-    time.sleep(0.5)
+    time.sleep(0.4)
     pydirectinput.press('space')
     time.sleep(0.5)
     pydirectinput.press('right', presses=2)
@@ -157,12 +162,58 @@ def mechanic():
     pydirectinput.press('space')
     time.sleep(0.1)
 
-
+def lester():
+    print("LOG >> KeyPressed F6 -> QuickLester")
+    pydirectinput.press('up')
+    time.sleep(0.5)
+    pydirectinput.press('up')
+    time.sleep(0.1)
+    pydirectinput.press('right')
+    pydirectinput.press('enter')
+    time.sleep(0.4)
+    pydirectinput.press('space')
+    time.sleep(0.5)
+    pydirectinput.press('left')
+    pydirectinput.press('enter')
+    time.sleep(0.1)
+    pydirectinput.press('right')
+    pydirectinput.press('down')
+    pydirectinput.press('enter')
+    time.sleep(0.1)
+    pydirectinput.press('left')
+    pydirectinput.press('enter')
+    time.sleep(0.1)
+    pydirectinput.press('left')
+    pydirectinput.press('enter')
+    time.sleep(0.1)
+    pydirectinput.press('enter')
+    time.sleep(0.1)
+    pydirectinput.press('enter')
+    time.sleep(0.1)
+    pydirectinput.press('down', presses=2)
+    pydirectinput.press('enter')
+    time.sleep(0.1)
+    pydirectinput.press('down')
+    pydirectinput.press('left')
+    pydirectinput.press('enter')
+    time.sleep(0.1)
+    pydirectinput.press('up')
+    pydirectinput.press('right')
+    pydirectinput.press('enter')
+    time.sleep(0.1)
+    pydirectinput.press('down')
+    pydirectinput.press('enter')
+    time.sleep(0.1)
+    pydirectinput.press('space')
+    time.sleep(0.1)
+    
 '''
 HotKey Detection
 '''
-keyboard.add_hotkey('f4', lambda: snack())
-keyboard.add_hotkey('f5', lambda: armor())
+keyboard.add_hotkey('f3', lambda: snack())
+keyboard.add_hotkey('f4', lambda: armor())
+keyboard.add_hotkey('f5', lambda: mechanic())
+keyboard.add_hotkey('f6', lambda: lester())
 keyboard.add_hotkey('f9', lambda: kill())
 keyboard.add_hotkey('f12', lambda: unblock())
 keyboard.add_hotkey('f11', lambda: block())
